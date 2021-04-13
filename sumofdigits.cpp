7870bac8a202709439e4d02e0b50df0544b3af87
Ronnie2015 <34644663+Ronnie2015@users.noreply.github.com>
2021-04-13 01:23:43 -0700

    Update sumofdigits.cpp

--- sumofdigits.cpp
+++ sumofdigits.cpp
@@ -1,3 +1,5 @@
+// Iterative
+
 #include<iostream>
 using namespace std;
 
@@ -16,3 +18,22 @@ int main(){
     cout<<SumOfDigits(n);
     return 0;
 }
+
+// recursive
+#include<iostream>
+using namespace std;
+
+int SumOfDigits(long n){
+    if (n <= 0) return 0;
+    return n%10 + SumOfDigits(n/10);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int sum = SumOfDigits(n);
+    cout<<sum;
+    return 0;
+}
+
+
